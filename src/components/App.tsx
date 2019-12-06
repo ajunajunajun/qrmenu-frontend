60e6d7895b7581877e8d5d029f5be6ac1af4a475
ajunajunajun <ajunnajunn@gmail.com>
2019-12-06 11:39:16 +0900

    change divide root shop

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { RootProvider } from '../store/RootContext'
+import { ShopProvider } from '../store/ShopContext'
 import NonPrivateRoute from '../helpers/NonPrivateRoute'
 import PrivateRoute from '../helpers/PrivateRoute'
 import IndexPage from './pages/IndexPage/IndexPage'
@@ -10,22 +11,28 @@ import AppHeader from './projects/AppHeader/AppHeader'
 
 export default () => (
   <RootProvider>
-    <BrowserRouter>
-      <AppHeader />
-      <Switch>
-        <NonPrivateRoute
-          redirect="/my"
-          path="/"
-          exact={true}
-          component={IndexPage}
-        />
-        <PrivateRoute redirect="/">
-          <Switch>
-            <Route path="/my" exact={true} component={MyPage} />
-            <Route path="/createmenu" exact={true} component={CreateMenuPage} />
-          </Switch>
-        </PrivateRoute>
-      </Switch>
-    </BrowserRouter>
+    <ShopProvider>
+      <BrowserRouter>
+        <AppHeader />
+        <Switch>
+          <NonPrivateRoute
+            redirect="/my"
+            path="/"
+            exact={true}
+            component={IndexPage}
+          />
+          <PrivateRoute redirect="/">
+            <Switch>
+              <Route path="/my" exact={true} component={MyPage} />
+              <Route
+                path="/createmenu"
+                exact={true}
+                component={CreateMenuPage}
+              />
+            </Switch>
+          </PrivateRoute>
+        </Switch>
+      </BrowserRouter>
+    </ShopProvider>
   </RootProvider>
 )
