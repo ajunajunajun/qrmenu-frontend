8b07f86456a442c12f1fbe1566200c3006428f09
ajunajunajun <ajunnajunn@gmail.com>
2020-01-30 01:10:37 +0900

    create  changevalue

--- src/components/projects/LoginForm/LoginForm.tsx
+++ src/components/projects/LoginForm/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, ChangeEvent } from 'react'
 import '../../../styles/projects/_loginform.scss'
 import { useRootContext } from '../../../store/RootContext'
 import { useShopContext } from '../../../store/ShopContext'
@@ -10,21 +10,35 @@ import Button from '../../ui/Button/Button'
  */
 export default function LoginForm() {
   const { isAuthenticated, setAuthenticate } = useRootContext()
-
   const { setShopdata } = useShopContext()
+
+  const [isId, setId] = useState('')
+  const [isPassword, setPassword] = useState('')
+
+  const ChangeId = (event: ChangeEvent<HTMLInputElement>) => {
+    setId(event.target.value)
+  }
+
+  const ChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value)
+  }
+
   const Login = () => {
+    console.log(isId, isPassword)
+    /*
     setAuthenticate(!isAuthenticated)
     setShopdata({
       shoplist: shoplistJson[0]
     })
+    */
   }
   return (
     <div className="p-loginform">
       <p>loginform</p>
       <p>id</p>
-      <input />
+      <input value={isId} onChange={event => ChangeId(event)} />
       <p>password</p>
-      <input />
+      <input value={isPassword} onChange={event => ChangePassword(event)} />
       <br />
       <Button onClick={Login}>Login</Button>
     </div>
