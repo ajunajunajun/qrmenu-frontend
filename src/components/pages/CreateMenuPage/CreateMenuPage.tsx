02fabbf8697a67c27d9695819e6f9ada94076182
ajunajunajun <ajunnajunn@gmail.com>
2020-01-30 04:39:34 +0900

    create menupage

--- src/components/pages/CreateMenuPage/CreateMenuPage.tsx
+++ src/components/pages/CreateMenuPage/CreateMenuPage.tsx
@@ -6,17 +6,16 @@ import '../../../styles/projects/_menuobject.scss'
 import CreateMenuHeader from '../../projects/CreateMenu/CreateMenuHeader/CreateMenuHeader'
 import CreateMenuSideBar from '../../projects/CreateMenu/CreateMenuSideBar/CreateMenuSideBar'
 import CreateMenu from '../../projects/CreateMenu/CreateMenu'
-import { MenuProvider } from '../../../store/MenuContext'
 
 /**
  * 初めに表示するページ
  */
 export default function CreateMenuPage() {
   return (
-    <MenuProvider>
+    <>
       <CreateMenuHeader />
       <CreateMenuSideBar />
       <CreateMenu />
-    </MenuProvider>
+    </>
   )
 }
